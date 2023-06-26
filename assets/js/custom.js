e308fb92d3c69b6394f5552bb63e7dc35bcea6cf
mephisto <mephisto>
2023-06-26 14:36:19 +0800

    go to top

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,11 @@
 // add custom js in this file
+
+window.onscroll = function () {
+  var pageOffset =
+    document.documentElement.scrollTop || document.body.scrollTop;
+  if (pageOffset > 1000) {
+    document.getElementById("toTop").style.visibility = "visible";
+  } else {
+    document.getElementById("toTop").style.visibility = "hidden";
+  }
+};
