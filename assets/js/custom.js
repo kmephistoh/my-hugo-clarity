fa3fc274f4b560dfce2a8e703a8cc685a1b1cb40
mephisto <mephisto>
2024-03-01 17:11:16 +0800

    update Adblocker checker

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,18 +10,34 @@ window.onscroll = function () {
   }
 };
 
-function checker() {
-  var isAdblocker = typeof adblock !== "undefined";
-  if (isAdblocker) {
-    var sponsor = document.querySelector("#my-content");
-    var prompt = document.createElement("div");
-    prompt.className = "content";
-    prompt.style =
-      "hborder: 1px solid #c6c6c6;border-radius: 4px;background-color: #f5f2f0;padding: 15px; font-size: 14px;";
-    prompt.innerHTML =
-      "<h1>您使用了广告拦截器，导致本站内容无法显示。</h1> <p>小站依赖广告维持运转，还请将 mephisto.cc 加入白名单，解除广告屏蔽后，刷新页面。谢谢!</p>";
-    sponsor.parentNode.replaceChild(prompt, sponsor);
+function detectAdblock() {
+  try {
+    fetch(
+      new Request(
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+        {
+          method: "HEAD",
+          mode: "no-cors",
+        }
+      )
+    ).catch((error) => {
+      showNotification();
+    });
+  } catch (e) {
+    // Request failed, likely due to ad blocker
+    showNotification();
   }
 }
 
-setTimeout(checker, 1000);
+function showNotification() {
+  var sponsor = document.querySelector("#my-content");
+  var prompt = document.createElement("div");
+  prompt.className = "content";
+  prompt.style =
+    "hborder: 1px solid #c6c6c6;border-radius: 4px;background-color: #f5f2f0;padding: 15px; font-size: 14px;";
+  prompt.innerHTML =
+    "<h1>您使用了广告拦截器，导致本站内容无法显示。</h1> <p>创作不易，小站依赖广告维持运转，还请将 mephisto.cc 加入白名单，解除广告屏蔽后，刷新页面。谢谢🙏</p>";
+  sponsor.parentNode.replaceChild(prompt, sponsor);
+}
+
+setTimeout(detectAdblock, 1000);
