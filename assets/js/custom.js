b9b2ad1c2c966e685255ddcb26db5d232f56ecb9
mephisto <mephisto>
2024-03-01 15:53:22 +0800

    isAdblocker checker

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,3 +9,19 @@ window.onscroll = function () {
     document.getElementById("toTop").style.visibility = "hidden";
   }
 };
+
+function checker() {
+  var isAdblocker = typeof adblock !== "undefined";
+  if (isAdblocker) {
+    var sponsor = document.querySelector("#my-content");
+    var prompt = document.createElement("div");
+    prompt.className = "content";
+    prompt.style =
+      "hborder: 1px solid #c6c6c6;border-radius: 4px;background-color: #f5f2f0;padding: 15px; font-size: 14px;";
+    prompt.innerHTML =
+      "<h1>您使用了广告拦截器，导致本站内容无法显示。</h1> <p>小站依赖广告维持运转，还请将 mephisto.cc 加入白名单，解除广告屏蔽后，刷新页面。谢谢!</p>";
+    sponsor.parentNode.replaceChild(prompt, sponsor);
+  }
+}
+
+setTimeout(checker, 1000);
