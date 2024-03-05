20b3f727ffa12a47a02ec939251d36ddbb7162f6
mephisto <mephisto>
2024-03-05 09:57:15 +0800

    change noti info

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -36,7 +36,7 @@ function showNotification() {
   prompt.style =
     "hborder: 1px solid #c6c6c6;border-radius: 4px;background-color: #f5f2f0;padding: 15px; font-size: 14px;";
   prompt.innerHTML =
-    "<h1>您使用了广告拦截器，导致本站内容无法显示</h1><h2> You are using an ad blocker, which prevents the content of this site from being displayed</h2> <p>创作不易，小站依赖广告维持运转，还请将 mephisto.cc 加入白名单，解除广告屏蔽后，刷新页面。谢谢🙏</p> <p>Please add mephisto.cc to the whitelist, unblock the advertising, and refresh the page. Thank you 🙏</p>";
+    "<h1>您使用了广告拦截器，导致本站内容无法显示</h1><h2> You are using an ad blocker, which prevents the content of this site from being displayed</h2> <p>创作不易，小站依赖广告维持运转，还请将 <font color='green'><strong>mephisto.cc</strong></font> 加入白名单，解除广告屏蔽后，刷新页面。谢谢🙏</p> <p>Please add <font color='green'><strong>mephisto.cc</strong></font> to the whitelist, unblock the advertising, and refresh the page. Thank you 🙏</p>";
   sponsor.parentNode.replaceChild(prompt, sponsor);
 }
 
