a728da5c8f321a3aa1a00543f0df878b98e95a3e
mephisto <kanhaosmile@gmail.com>
2025-11-15 13:50:06 +0800

    去除广告屏蔽检测

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,34 +10,35 @@ window.onscroll = function () {
   }
 };
 
-function detectAdblock() {
-  try {
-    fetch(
-      new Request(
-        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
-        {
-          method: "HEAD",
-          mode: "no-cors",
-        }
-      )
-    ).catch((error) => {
-      showNotification();
-    });
-  } catch (e) {
-    // Request failed, likely due to ad blocker
-    showNotification();
-  }
-}
+// 去除广告拦截检测，没什么意义
+// function detectAdblock() {
+//   try {
+//     fetch(
+//       new Request(
+//         "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+//         {
+//           method: "HEAD",
+//           mode: "no-cors",
+//         }
+//       )
+//     ).catch((error) => {
+//       showNotification();
+//     });
+//   } catch (e) {
+//     // Request failed, likely due to ad blocker
+//     showNotification();
+//   }
+// }
 
-function showNotification() {
-  var sponsor = document.querySelector("#my-content");
-  var prompt = document.createElement("div");
-  prompt.className = "content";
-  prompt.style =
-    "hborder: 1px solid #c6c6c6;border-radius: 4px;background-color: #f5f2f0;padding: 15px; font-size: 14px;";
-  prompt.innerHTML =
-    "<h1>您使用了广告拦截器，导致本站内容无法显示</h1><h2> You are using an ad blocker, which prevents the content of this site from being displayed</h2> <p>创作不易，小站依赖广告维持运转，还请将 <font color='green'><strong>mephisto.cc</strong></font> 加入白名单，解除广告屏蔽后，刷新页面。谢谢🙏</p> <p>Please add <font color='green'><strong>mephisto.cc</strong></font> to the whitelist, unblock the advertising, and refresh the page. Thank you 🙏</p> <img src='/images/unblock-ads.webp'/> <img src='/images/whitelist.webp'/>";
-  sponsor.parentNode.replaceChild(prompt, sponsor);
-}
+// function showNotification() {
+//   var sponsor = document.querySelector("#my-content");
+//   var prompt = document.createElement("div");
+//   prompt.className = "content";
+//   prompt.style =
+//     "hborder: 1px solid #c6c6c6;border-radius: 4px;background-color: #f5f2f0;padding: 15px; font-size: 14px;";
+//   prompt.innerHTML =
+//     "<h1>您使用了广告拦截器，导致本站内容无法显示</h1><h2> You are using an ad blocker, which prevents the content of this site from being displayed</h2> <p>创作不易，小站依赖广告维持运转，还请将 <font color='green'><strong>mephisto.cc</strong></font> 加入白名单，解除广告屏蔽后，刷新页面。谢谢🙏</p> <p>Please add <font color='green'><strong>mephisto.cc</strong></font> to the whitelist, unblock the advertising, and refresh the page. Thank you 🙏</p> <img src='/images/unblock-ads.webp'/> <img src='/images/whitelist.webp'/>";
+//   sponsor.parentNode.replaceChild(prompt, sponsor);
+// }
 
-setTimeout(detectAdblock, 1000);
+// setTimeout(detectAdblock, 1000);
